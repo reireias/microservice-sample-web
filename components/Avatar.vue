1a4234f5cdec24f47bd00d9f8dd8653231f3f7d9
reireias <reireias@gmail.com>
2019-03-12 23:27:06 +0900

    add layout and store

---
+++ components/Avatar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="avatar">
+    <template v-if="$store.state.auth">
+      <v-menu bottom left>
+        <v-btn slot="activator" icon>
+          <v-avatar size="32px">
+            <img :src="$store.state.user.avatarUrl">
+          </v-avatar>
+        </v-btn>
+        <v-list>
+          <v-list-tile href="/api/auth/logout">
+            <v-list-tile-title>Logout</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </template>
+    <template v-else>
+      <v-btn
+        round
+        outline
+        href="/api/auth/github"
+      >
+        Login
+      </v-btn>
+    </template>
+  </div>
+</template>
+
+
+<style>
+.v-tabs .v-btn {
+  text-transform: none;
+}
+.avatar {
+  align-self: center;
+}
+.v-menu__activator {
+  height: 100%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,40 @@
 <template>
-  <nuxt />
+  <v-app>
+    <v-tabs
+      icons-and-text
+      dark
+      color="primary"
+      slider-color="accent"
+    >
+      <v-tab to="/home">
+        Home
+        <v-icon>home</v-icon>
+      </v-tab>
+      <v-tab to="/users">
+        Users
+        <v-icon>people</v-icon>
+      </v-tab>
+      <v-spacer />
+      <avatar />
+    </v-tabs>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+import Avatar from '~/components/Avatar.vue'
+export default {
+  components: {
+    Avatar
+  },
+  data() {
+    return {
+      title: 'Microservice Sample'
+    }
+  }
+}
+</script>
