eb9dbed90a127f9c51b89699b7002751ecdc0856
reireias <reireias@gmail.com>
2019-03-12 23:58:38 +0900

    add oauth

--- server/index.js
+++ server/index.js
@@ -1,7 +1,75 @@
 const express = require('express')
-const consola = require('consola')
+const morgan = require('morgan')
+const session = require('express-session')
+const passport = require('passport')
+const { Strategy } = require('passport-github2')
+const bodyParser = require('body-parser')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+
+app.use(morgan('short'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: 'auto'
+    }
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL || 'http://localhost:3000/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        const name = profile.username
+        const avatarUrl = profile.photos[0].value
+        // TODO: create or update user data
+        return done(null, {
+          _id: 'dummy',
+          name: name,
+          avatarUrl: avatarUrl
+        })
+      })
+    }
+  )
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
+
+app.get(
+  '/api/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+)
+app.get(
+  '/api/auth/github/callback',
+  passport.authenticate('github'),
+  (req, res) => {
+    res.json({ user: req.user })
+  }
+)
+app.get('/api/auth/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+app.get('/api/session', (req, res) => {
+  res.json({ user: req.user })
+})
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -11,7 +79,10 @@ async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const {
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000
+  } = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
@@ -26,9 +97,5 @@ async function start() {
 
   // Listen the server
   app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
 }
 start()
